d8533085984b695c9fceed22f528db40f8dce101
Maria Ilieva <mila234@gmail.com>
2022-12-10 18:19:35 +0200

    Tests

--- employees/employees/departments_app/tests.py
+++ employees/employees/departments_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from employees.departments_app.models import Department
 
-# Create your tests here.
+
+class DepartmentTest(TestCase):
+    def setUp(self):
+        self.dep1 = Department.objects.create(title='IT', category='C2')
+
+    def test_department_created_success(self):
+        self.assertEqual(self.dep1.title, 'IT')
+        self.assertEqual(self.dep1.category, 'C2')
--- employees/employees/employees_app/tests.py
+++ employees/employees/employees_app/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import get_user_model, authenticate
 from django.test import TestCase
 
-# Create your tests here.
+
+class SigninTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='test', password='12test12', email='test@example.com',
+                                                         age=22, first_name='Test', last_name='Testov',
+                                                         phone_number='0987654321', department_id=6)
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_correct(self):
+        user = authenticate(username='test', password='12test12')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_wrong_username(self):
+        user = authenticate(username='wrong', password='12test12')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_wrong_password(self):
+        user = authenticate(username='test', password='wrong')
+        self.assertFalse(user is not None and user.is_authenticated)
